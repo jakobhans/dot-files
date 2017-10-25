cd31293790de0a3483d84d742a0866d6d7d99be8
Jakob H. Renpening <jakobhans@gmail.com>
2017-10-24 22:57:01 -0500

    Added some new stuff for filesystem monitoring

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -4,5 +4,6 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 MONITOR=HDMI-0 polybar top &
+MONITOR=DP-4 polybar top &
 MONITOR=DP-2 polybar top &
 MONITOR=DP-0 polybar top
